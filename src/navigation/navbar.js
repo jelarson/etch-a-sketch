c0e0127e6c32cc0722319b5becd829e8b37deb4c
jelarson <jesslarson93@gmail.com>
2020-11-11 12:39:04 -0700

    updated tab title, navbar styles

--- src/navigation/navbar.js
+++ src/navigation/navbar.js
@@ -13,26 +13,28 @@ const navbarWrapperCss = css`
 const navbarTitleCss = css`
   font-size: 1.3em;
   margin-top: 10px;
+  margin-bottom: 10px;
+  color: white;
 `
 
-const navbarLinksWrapperCss = css`
-  display: flex;
-  flex-direction: row;
-  width: 60%;
-  justify-content: space-between;
-  margin-top: 12px;
-  margin-bottom: 12px;
-`
+// const navbarLinksWrapperCss = css`
+//   display: flex;
+//   flex-direction: row;
+//   width: 60%;
+//   justify-content: space-between;
+//   margin-top: 12px;
+//   margin-bottom: 12px;
+// `
 
 export default function Navbar() {
   return (
     <div css={navbarWrapperCss}>
       <div css={navbarTitleCss}>Etch-A-Sketch!</div>
-      <div css={navbarLinksWrapperCss}>
+      {/* <div css={navbarLinksWrapperCss}>
         <NavLink path="/link1" buttonText="Link #1" />
         <NavLink path="/link2" buttonText="Link #2" />
         <NavLink path="/link3" buttonText="Link #3" />
-      </div>
+      </div> */}
     </div>
   )
 }
